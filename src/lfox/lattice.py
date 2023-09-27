a32c3490e08ac81f6b20879af7aa88df6403db75
Ethan Neil <ethan.neil@colorado.edu>
2023-09-27 17:21:41 -0600

    Speed up copying fields

--- src/lfox/lattice.py
+++ src/lfox/lattice.py
@@ -120,7 +120,9 @@ class LatticeField:
         cls = self.__class__
         new = cls.__new__(cls)
         new.__dict__.update(self.__dict__)
-        new.field = jnp.copy(self.field)
+# JAX/NumPy arrays are immutable, and this was slow for some reason
+#        new.field = jnp.copy(self.field)
+        new.field = self.field  
 
         return new
     
