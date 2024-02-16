2ee033b4007f32f440ac32333212628ee610a208
Ethan Neil <ethan.neil@colorado.edu>
2024-02-16 13:53:40 -0800

    Physics ex working again!

--- src/lfox/evolution/hmc.py
+++ src/lfox/evolution/hmc.py
@@ -335,27 +335,119 @@ from equinox import AbstractVar
 
 class EvolverRewrite(eqx.Module):
     action: Action
-    seed: int
-    fields: dict
-    observables: dict
-    save_freq: AbstractVar[int]
-
-    # Should these not be dataclass properties?
-    #rng_key: jax.random.PRNGKey
+    # save_freq: AbstractVar[int]
 
     @abstractmethod
-    def evolve(self):
-        pass    
+    def evolve(self, field, warmup=False):
+        pass
 
 
-
-
-class HMCRewrite(EvolverRewrite):
+class HMCRewrite(eqx.Module):
+    action: Action
     integrator: MDIntegrator
-    save_freq: int = 1
 
-    def evolve(self):
-        print("OK")
+    def H_density(self, S_field, pi_fields):
+        H_field = S_field.copy()
+        for fname in pi_fields.keys():
+            H_field += 0.5 * pi_fields[fname]**2
+        
+        return H_field
+
+    def H(self, S_field, pi_fields):
+        H_field = self.H_field(S_field, pi_fields)
+
+        return jnp.sum(H_field.F)
+
+    def momentum_refresh(self, fields, rng_key):
+        pi_fields = {}
+        for fname in fields.keys():
+            rng_key, subkey = jax.random.split(rng_key)
+            pi_fields[fname] = jax.random.normal(subkey, shape=fields[fname].F.shape)
+
+        return pi_fields, rng_key
+
+    def delta_fields(self):
+        def delta_X(X, P):
+            return P
+        
+        return delta_X
+
+    def delta_mom(self):
+        def delta_P(X, P):
+            result = {}
+            derivs = self.action.dS(X)
+            for field in self.action.field_names:
+                result[field] = -1 * derivs[field]
+
+            return result
+        
+        return delta_P
+
+    def MD_traj(self, fields, pi_fields):
+        S_old = self.action.S_field(fields)
+        H_old = self.H_density(S_old, pi_fields)
+
+        fields, pi_fields = self.integrator.integrate(
+            delta_X = self.delta_fields(),
+            delta_P = self.delta_mom(),
+            X = fields,
+            P = pi_fields,
+        )
+
+        S_new = self.action.S_field(fields)
+        H_new = self.H_density(S_new, pi_fields)
+
+        delta_H = jnp.sum(H_new.F - H_old.F)
+        P_acc = jnp.exp(-delta_H)
+
+        return fields, pi_fields, delta_H, P_acc
+
+    @partial(jax.jit, static_argnums=(3,4,))
+    def evolve(self, fields, rng_key, warmup=False, return_pi=False):
+
+        monitor = {}
+
+        # Refresh momentum
+        pi_fields, new_rng_key = self.momentum_refresh(fields, rng_key)
+
+        # Integrate forward
+        new_fields, new_pi_fields, delta_H, P_acc = self.MD_traj(fields, pi_fields)
+
+        monitor['delta_H'] = delta_H
+        monitor['P_acc'] = P_acc
+
+        accept = True
+        if not warmup:  # Warmups always accept!
+            new_rng_key, subkey = jax.random.split(rng_key)
+            r = jax.random.uniform(subkey)
+
+            accept = jax.lax.cond(r < P_acc, lambda _: True, lambda _: False, r)
+            new_fields = jax.lax.cond(r < P_acc, lambda _: new_fields, lambda _: fields, r)
+
+        monitor['accept'] = accept
+
+        return new_fields, monitor, new_rng_key
+    
+    @partial(jax.jit, static_argnums=(3,4,))
+    def evolve_many(self, fields, rng_key, traj=1, warmup=False):
+        global_monitor = {
+            'delta_H': jnp.array([jnp.nan]*traj),
+            'P_acc': jnp.array([jnp.nan]*traj),
+            'accept': jnp.array([jnp.nan]*traj),
+        }
+
+        def ev_loop(i, inputs):
+            fields, rng_key, monitor = inputs
+            fields, mon_step, rng_key = self.evolve(fields, rng_key, warmup=warmup)
+            for key in ('delta_H', 'P_acc', 'accept'):
+                monitor[key] = monitor[key].at[i].set(mon_step[key])
+
+            return (fields, rng_key, monitor)
+
+        fields, rng_key, global_monitor = jax.lax.fori_loop(0, traj, ev_loop, (fields, rng_key, global_monitor))
+
+        return fields, global_monitor, rng_key
+
 
 
 
